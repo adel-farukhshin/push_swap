cdfd2ffb1d42ae1e42051ac9f2051c8b2499e13e
adelfar <afarukhshin@gmail.com>
2022-02-15 11:13:11 +0300

    feat: find_extr is added

--- sort.c
+++ sort.c
@@ -1,7 +1,28 @@
 
 #include "push_swap.h"
 
+#include <stdio.h>
+
+void find_extr(t_stack *a, int *min, int *max)
+{
+	*min = a->data;
+	*max = a->data;
+	while (a)
+	{
+		if (a->data > *max)
+			*max = a->data;
+		if (a->data < *min)
+			*min = a->data;
+		a = a->next;
+	}
+}
+
 void sort(t_stack *a, t_stack *b)
 {
-	
+	int	min;
+	int	max;
+
+	find_extr(a, &min, &max);
+	printf("min: %d; max: %d \n", min, max);
+	b = (void *)b;
 }
