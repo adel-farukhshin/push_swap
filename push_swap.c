0aa0c4f7f2b40df3d677c26e5eac56bd73437aed
adelfar <afarukhshin@gmail.com>
2022-02-15 09:46:45 +0300

    fix: stack_init is added

--- push_swap.c
+++ push_swap.c
@@ -11,11 +11,14 @@
 
 int	main (int argc, char **argv)
 {
-	int	i;
+	int		i;
+	t_stack	*a;
 
 	i = check(argc, argv);
 	printf("check out %d\n", i);
 	if (i == -1)
 		return (-1);
-	stack_init(argc, argv);
+	a = stack_init(argc, argv);
+	stack_printf(a);
+
 }
