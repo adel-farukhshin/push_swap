0bd9deb08a3ea3f74be91feb00fb4a5d947bb117
Butcher Sarai <bsarai@il-k1.msk.21-school.ru>
2022-02-21 13:08:51 +0300

    feat: add fill_b function

--- sort.c
+++ sort.c
@@ -29,41 +29,37 @@ void find_extr(t_stack *a, int *min, int *max)
 	}
 }
 
-void sort(t_stack *a, t_stack *b, int argc)
+void	fill_b(t_stack **a, t_stack **b, int argc)
 {
 	int	min;
 	int	max;
 	int	i;
-	// t_stack *t_a;
-	// t_stack *t_b;
 
 	i = 1;
-	find_extr(a, &min, &max);
+	find_extr((*a), &min, &max);
 	printf("min: %d; max: %d \n", min, max);
-	// t_a = a;
-	// t_b = b;
+	// printf("b address: %p\n", (*b));
 	while (i < argc)
 	{
-		
-
-
-		// if ((a->data == min || a->data == max) && i + 1 < argc)
-		// 	r(&a);
-		// else
-		// 	p(&a, &b);
-
-		if (a->data != min && a->data != max)
-			p(&a, &b);
+		if ((*a)->data != min && (*a)->data != max)
+			p(a, b);
 		else if (i + 1 < argc)
-			r(&a);
-	
-			
-
-		// t_a = t_a->next;
-		stacks_printf(a, b);
+			r(a);
+		stacks_printf((*a), (*b));
 		i++;
-		// a = a->next;
 	}
+	// printf("b address after while: %p\n", (*b));
+
+}
+
+void sort(t_stack *a, t_stack *b, int argc)
+{
+	
+	fill_b(&a, &b, argc);
+	// printf("b address after fill_b: %p\n", b);
+	printf("after fill b\n");
+	stacks_printf(a, b);
+
 	// stacks_printf(a, b);
 	
 	// s(&a);
