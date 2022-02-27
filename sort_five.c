85117b23279ff5336b5f19ecf53870b28088c583
Butcher Sarai <bsarai@il-j1.msk.21-school.ru>
2022-02-27 16:58:40 +0300

    refactor: change ismaxinstack, so it will check whether max in stack a

--- sort_five.c
+++ sort_five.c
@@ -13,19 +13,19 @@
 #include "push_swap.h"
 #include "ft_printf.h"
 
-int	ismaxinstack(t_stacks *stacks, int max)
-// int	ismaxinstack(t_stacks *stacks, int val)
+// int	ismaxinstack(t_stacks *stacks, int max)
+static int	ismaxinstack(t_stacks *stacks, int val)
 {
 	t_stack	*tmp;
 
-	tmp = stacks->b;
+	tmp = stacks->a;
 	while (tmp)
 	{
-		if (tmp->data == max)
-			return (1);
+		if (tmp->data > val)
+			return (0);
 		tmp = tmp->next;
 	}
-	return (0);
+	return (1);
 }
 
 void	sort_five(t_stacks *stacks, int *arr)
@@ -34,26 +34,23 @@ void	sort_five(t_stacks *stacks, int *arr)
 	int	med;
 	int	max;
 
-
 	// ft_printf("i %d %d \n", stacks->la, arr[0]);
-
-	stacks_printf(stacks->a, stacks->b);
+	// stacks_printf(stacks->a, stacks->b);
 	find_ms(arr, &min, &med, &max);
 	while (stacks->la != 3)
 		pb(stacks);
 	sort_tripple(stacks);
-	stacks_printf(stacks->a, stacks->b);
+	// stacks_printf(stacks->a, stacks->b);
 	if (!is_final(stacks->b))
 		rb(stacks);
-	stacks_printf(stacks->a, stacks->b);
+	// stacks_printf(stacks->a, stacks->b);
 	while (stacks->b)
 	{
 		if ((stacks->b)->data < (stacks->a)->data)
 			pa(stacks);
-		else if (ismaxinstack(stacks, max) && (stacks->a)->data == min)
+		else if (ismaxinstack(stacks, (stacks->b)->data) && (stacks->a)->data == min)
 			pa(stacks);
 		ra(stacks);
-
-		stacks_printf(stacks->a, stacks->b);
+		// stacks_printf(stacks->a, stacks->b);
 	}
 }
