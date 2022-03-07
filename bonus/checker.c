b893e6716edc6b4b9c73678e52464812f16c822c
Butcher Sarai <bsarai@il-k1.msk.21-school.ru>
2022-03-07 13:58:20 +0300

    refactor: delete parse from main

--- bonus/checker.c
+++ bonus/checker.c
@@ -16,35 +16,11 @@
 #include "get_next_line.h"
 #include <stdlib.h>
 
-void	parse(t_stacks *stacks)
-{
-	char		*str;
-	char		*p;
-	char		*model = "sa\nsb\npa\npb\nra\nrb\nrr\nrra\nrrb\nrrr\n";
-
-	str = get_next_line(0);
-	while (str)
-	{
-		p = ft_strstr(model, str);
-		if (!p)
-		{
-			ft_printf("Error\n");
-			exit(-1);
-		}
-		
-		ft_printf("%s", p);
-		free (str);
-		str = get_next_line(0);
-		
-	}
-	stacks_printf(stacks->a, stacks->b);
-}
 
 int	main(int argc, char **argv)
 {
 	t_stacks	*stacks;
 	
-
 	check(argc, argv);
 	stacks = stacks_init(argc, argv);
 	parse(stacks);
