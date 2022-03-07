0f364141e093700063e2b8d9c93bcecf4af46bff
Butcher Sarai <bsarai@il-k1.msk.21-school.ru>
2022-03-07 14:31:41 +0300

    feat: add check_sort in checker

---
+++ bonus/check_sort.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsarai <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 12:03:41 by bsarai            #+#    #+#             */
+/*   Updated: 2022/02/21 12:03:42 by bsarai           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_final(t_stack *a)
+{
+	t_stack	*temp;
+
+	while (a->next)
+	{
+		temp = a;
+		a = a->next;
+		if (temp->data > a->data)
+			return (0);
+	}
+	return (1);
+}
+
+// int	is_sorted(t_stack *a)
+// {
+// 	int		i;
+// 	t_stack	*temp;
+// 	t_stack	*head;
+
+// 	i = 0;
+// 	head = a;
+// 	while (a->next)
+// 	{
+// 		temp = a;
+// 		a = a->next;
+// 		if (temp->data > a->data)
+// 			i++;
+// 	}
+// 	if (!i || (i == 1 && a->data < head->data))
+// 		return (1);
+// 	else
+// 		return (0);
+// }
